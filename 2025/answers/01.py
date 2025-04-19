0d7008df85742ebaea9fa246a8985aa3a653b3b3
choiseungje <161275179+choiseungje@users.noreply.github.com>
2025-04-18 21:17:53 -0700

    style: Change to comment in code

--- 2025/answers/01.py
+++ 2025/answers/01.py
@@ -8,17 +8,17 @@ if odd_arr:
 else:
     print(-1)
 ##########################################################################
-arr = []
-for i in range(7):
-    arr.append(int(input()))
+# arr = []
+# for i in range(7):
+#     arr.append(int(input()))
 
-odd_arr = []
-for i in arr:
-    if i % 2 == 0:
-        odd_arr.append(i)
+# odd_arr = []
+# for i in arr:
+#     if i % 2 == 0:
+#         odd_arr.append(i)
 
-if len(odd_arr) != 0:
-    print(sum(odd_arr))
-    print(min(odd_arr))
-else:
-    print(-1)
+# if len(odd_arr) != 0:
+#     print(sum(odd_arr))
+#     print(min(odd_arr))
+# else:
+#     print(-1)
